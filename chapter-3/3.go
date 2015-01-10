24c6ec4c06ad38895b73cb4bd8bb715d1ee3ea95
Hugh Grigg <hugh.grigg@gmail.com>
2015-01-10 15:18:55 +0000

    Problem 3 from Chapter 3

---
+++ chapter-3/3.go
@@ -0,0 +1,14 @@
+/* http://www.golang-book.com/3/index.htm
+Although overpowered for the task you can use Go as a calculator. Write a
+program that computes 32132 × 42452 and prints it to the terminal. (Use the *
+operator for multiplication) */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(32132 * 42452)
+}
+
+// = 1364067664
